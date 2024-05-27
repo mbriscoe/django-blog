aac77fc95b6ee566041dd65c72cf622aa8cbafd9
Mark Briscoe <mark@mcmw.co.uk>
2024-05-27 11:03:19 +0000

    -amend

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,10 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.path.isfile("env.py"):
-    DEBUG = True
-else:
-    DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com", ".gitpod.io"]
 
