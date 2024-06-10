f19c05d61e35cf345de794d9f5db327bd7eacc54
Mark Briscoe <mark@mcmw.co.uk>
2024-06-10 19:13:03 +0000

    add: print statements to display order of execution

--- blog/views.py
+++ blog/views.py
@@ -33,6 +33,7 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
 
     if request.method == "POST":
+        print("received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -45,7 +46,7 @@ def post_detail(request, slug):
             )
 
     comment_form = CommentForm()
-
+    print("about to render template")
     return render(
         request,
         "blog/post_detail.html",
